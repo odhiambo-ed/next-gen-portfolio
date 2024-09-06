b14f85167045a19df01cd8854f21bf038689e6bf
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-06 23:22:35 +0300

    add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center align-items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-white">Jesus I trust in You</h1>
+        <Hero />
       </div>
     </main>
   );
