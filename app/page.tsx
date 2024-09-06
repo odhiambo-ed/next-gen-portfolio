e69a0bb9be7d5a620d68eec292756ae44f5a0ed0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-06 23:20:58 +0300

    add style

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <main>
-      <h1>Jesus, in You I trust.</h1>
+    <main className="relative bg-black flex justify-center align-items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1 className="text-white">Jesus I trust in You</h1>
+      </div>
     </main>
   );
 }
