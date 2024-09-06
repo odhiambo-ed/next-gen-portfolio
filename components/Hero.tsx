3233c5e2f4d3bcae22f8110315c1193147bd61f9
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-06 23:52:07 +0300

    add spotlight to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Spotlight } from './ui/Sportlight'
 
 function Hero() {
   return (
       <div className='pt-30 pb-20'>
           <div>
-              Hero
+              <Spotlight className='h-screen -top-40 -left-10' fill='white' />
+              <Spotlight className='h-[60vh] w-[50vw] top-20 left-70' fill='red' />
+              <Spotlight className='h-[30vh] top-10' fill='blue' />
           </div>
     </div>
   )
